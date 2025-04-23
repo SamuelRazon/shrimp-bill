f09dc321a9ad60b8dd3af159fa0ca31a3965279e
EmilianoTH <emiliano.delatorre9058@alumnos.udg.mx>
2025-04-22 23:40:45 -0600

    [Backend](feat) -> Usos de DTO para mandar y recibir informacion importante por red de forma segura, este ademas tiene la funcion de escalabilidad siendo submodelos de los modelos principales al elegir tu lo que ocupas.

---
+++ Backend/bill-api/src/main/java/com/shrimpbill/bill_api/dto/UsuarioDto.java
@@ -0,0 +1,50 @@
+package com.shrimpbill.bill_api.dto;
+
+public class UsuarioDto {
+    private long id;
+    private String nombre;
+    private String apellidos;
+    private String email;
+
+    // Constructor completo
+    public UsuarioDto(long id, String nombre, String apellidos, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+    }
+
+    // Getters
+    public long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
