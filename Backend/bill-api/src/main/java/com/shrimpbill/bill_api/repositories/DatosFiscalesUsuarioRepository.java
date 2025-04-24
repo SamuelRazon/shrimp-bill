62ee945178b153d33409d33839c5f58484a1840f
EmilianoTH <emiliano.delatorre9058@alumnos.udg.mx>
2025-04-23 21:45:28 -0600

    [Backend](feat) -> Creacion de los repositorios necesarios para la creacion de facturas con los datos del usuario guardados.

---
+++ Backend/bill-api/src/main/java/com/shrimpbill/bill_api/repositories/DatosFiscalesUsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.shrimpbill.bill_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shrimpbill.bill_api.models.DatosFiscalesUsuarioModel;
+
+@Repository
+public interface DatosFiscalesUsuarioRepository
+        extends JpaRepository<DatosFiscalesUsuarioModel, Long> {
+    // De momento no ocupamos mas metodos
+}
